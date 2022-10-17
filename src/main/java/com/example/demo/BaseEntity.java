4166b4d23116c003f0952a7f3f8d1590fb350908
brightGarden02 <rreone2017@gmail.com>
2022-10-17 11:40:08 +0900

    feat: BaseEntity에 기능 추가

--- src/main/java/com/example/demo/BaseEntity.java
+++ src/main/java/com/example/demo/BaseEntity.java
@@ -1,14 +1,19 @@
 package com.example.demo;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
