2e1c7d0ce9da2216b888bf6c88bdd962f5a33e5a
JuWon Lee <rreone2017@gmail.com>
2022-10-24 16:47:20 +0900

    Delete src directory

--- src/main/java/com/example/demo/post/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.post.repository;
-
-import com.example.demo.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
